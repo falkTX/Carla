92cea703ba7c70672f537c246c56f01f5a2194ee
falkTX <falktx@gmail.com>
2013-01-22 21:10:27 +0000

    Update DPT license to LGPL

--- source/libs/distrho-plugin-toolkit/src/DistrhoUIDSSI.cpp
+++ source/libs/distrho-plugin-toolkit/src/DistrhoUIDSSI.cpp
@@ -3,16 +3,15 @@
  * Copyright (C) 2012-2013 Filipe Coelho <falktx@falktx.com>
  *
  * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU General Public License as
- * published by the Free Software Foundation; either version 2 of
- * the License, or any later version.
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
  *
- * For a full copy of the license see the GPL.txt file
+ * For a full copy of the license see the LGPL.txt file
  */
 
 #include "DistrhoDefines.h"
