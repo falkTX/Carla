96e79cbce15f42582a0d256eb66b1570150cf864
falkTX <falktx@gmail.com>
2013-02-12 11:20:02 +0000

    Update all headers

--- source/backend/plugin/fluidsynth.cpp
+++ source/backend/plugin/fluidsynth.cpp
@@ -1,18 +1,18 @@
 ﻿/*
- * Carla Backend
- * Copyright (C) 2011-2012 Filipe Coelho <falktx@falktx.com>
+ * Carla FluidSynth Plugin
+ * Copyright (C) 2011-2013 Filipe Coelho <falktx@falktx.com>
  *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License, or any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * GNU General Public License for more details.
  *
- * For a full copy of the GNU General Public License see the COPYING file
+ * For a full copy of the GNU General Public License see the GPL.txt file
  */
 
 #include "carla_plugin_internal.hpp"
