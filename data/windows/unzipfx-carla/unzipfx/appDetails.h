2ce95d6318aef8018e33df421a33f9e6d954cd1b
falkTX <falktx@gmail.com>
2013-03-22 05:04:03 +0000

    Add unzipfx code used for win32 build

---
+++ data/windows/unzipfx-carla/unzipfx/appDetails.h
@@ -0,0 +1,22 @@
+
+#ifndef __APP_DETAILS_H__
+#define __APP_DETAILS_H__
+
+#define SFX_APP_VERSION "0.6.0"
+#define SFX_APP_BANNER  "Carla self-contained executable " SFX_APP_VERSION ", based on UnZipSFX."
+
+#ifndef SFX_APP_MININAME
+# define SFX_APP_MININAME "Carla"
+#endif
+
+#ifdef WIN32
+# define SFX_AUTORUN_CMD  "\\" SFX_APP_MININAME ".exe"
+#else
+# define SFX_AUTORUN_CMD  "./" SFX_APP_MININAME
+#endif
+
+void  sfx_app_set_args(int argc, char** argv);
+int   sfx_app_autorun_now();
+char* sfx_get_tmp_path(int withAppName);
+
+#endif // __APP_DETAILS_H__
