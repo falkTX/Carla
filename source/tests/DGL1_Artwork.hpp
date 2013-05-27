58d5d6900d65ad87280f480c0205019ab4fbfd11
falkTX <falktx@gmail.com>
2013-05-27 19:49:41 +0100

    Update dgl test code

---
+++ source/tests/DGL1_Artwork.hpp
@@ -0,0 +1,15 @@
+/* (Auto-generated binary data file). */
+
+#ifndef BINARY_DGL1_ARTWORK_HPP
+#define BINARY_DGL1_ARTWORK_HPP
+
+namespace DGL1_Artwork
+{
+    extern const char* start_here_kxstudioData;
+    const unsigned int start_here_kxstudioDataSize = 262144;
+    const unsigned int start_here_kxstudioWidth    = 256;
+    const unsigned int start_here_kxstudioHeight   = 256;
+}
+
+#endif // BINARY_DGL1_ARTWORK_HPP
+
