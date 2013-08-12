c271861bbd60ce1022a01acab1b524e0c2e391cb
falkTX <falktx@gmail.com>
2013-08-12 20:57:15 +0100

    Import juce_audio_basics code

---
+++ source/modules/juce_audio_basics/AppConfig.h
@@ -0,0 +1,14 @@
+/*
+  ==============================================================================
+
+   Build options for juce_audio_basics static library
+
+  ==============================================================================
+*/
+
+#ifndef CARLA_JUCE_AUDIO_BASICS_APPCONFIG_H_INCLUDED
+#define CARLA_JUCE_AUDIO_BASICS_APPCONFIG_H_INCLUDED
+
+#include "../juce_core/AppConfig.h"
+
+#endif // CARLA_JUCE_AUDIO_BASICS_APPCONFIG_H_INCLUDED
