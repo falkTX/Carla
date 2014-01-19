d5cac4554ba5752e6de880b8923a1df38fe3c6da
falkTX <falktx@gmail.com>
2014-01-19 14:46:34 +0000

    Add build-haiku.sh script

---
+++ data/build-haiku.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+export HAIKU=true
+export CC=gcc-x86
+export CXX=g++-x86
+export MOC_QT4=moc-x86
+export RCC_QT4=rcc-x86
+export UIC_QT4=uic-x86
+export PKG_CONFIG_PATH=/boot/home/config/develop/lib/x86/pkgconfig:/boot/system/non-packaged/lib/pkgconfig
+
+exec "$@"
