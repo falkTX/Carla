42390dbdbdb1445071a105a26c4b2050e255111f
falkTX <falktx@gmail.com>
2014-02-08 07:56:45 +0000

    Misc

--- source/externalui.py
+++ source/externalui.py
@@ -1,19 +1,20 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-# DISTRHO Plugin Toolkit (DPT)
-# Copyright (C) 2012-2013 Filipe Coelho <falktx@falktx.com>
+# External UI
+# Copyright (C) 2013-2014 Filipe Coelho <falktx@falktx.com>
 #
-# Permission to use, copy, modify, and/or distribute this software for any purpose with
-# or without fee is hereby granted, provided that the above copyright notice and this
-# permission notice appear in all copies.
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation; either version 2 of
+# the License, or any later version.
 #
-# THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD
-# TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN
-# NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL
-# DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER
-# IN AN ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN
-# CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# For a full copy of the GNU General Public License see the doc/GPL.txt file.
 
 # -----------------------------------------------------------------------
 # Imports
