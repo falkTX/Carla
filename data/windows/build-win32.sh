da8da5ca2a3506a3e306d541c217d842b78543e6
falkTX <falktx@gmail.com>
2014-02-15 21:40:08 +0000

    Update build script

--- data/windows/build-win32.sh
+++ data/windows/build-win32.sh
@@ -25,8 +25,7 @@ export WINDRES=$MINGW-windres
 
 export CFLAGS="-DPTW32_STATIC_LIB -I$MINGW_PATH/include"
 export CXXFLAGS="-DPTW32_STATIC_LIB -DFLUIDSYNTH_NOT_A_DLL -I$MINGW_PATH/include"
-export EXTRA_LIBS1="-lglib-2.0 -lgthread-2.0 -lgig -lsndfile -lFLAC -lvorbisenc -lvorbis -logg -ldsound -lole32 -lrpcrt4 -lws2_32"
-export EXTRA_LIBS2="$MINGW_PATH/lib/qt4/plugins/imageformats/libqsvg.a"
+export EXTRA_LIBS="-lglib-2.0 -lgthread-2.0 -lgig -lsndfile -lFLAC -lvorbisenc -lvorbis -logg -ldsound -lole32 -lrpcrt4 -lws2_32 -lwinmm"
 
 export WINEARCH=win32
 export WINEPREFIX=~/.winepy3
@@ -43,27 +42,27 @@ make clean
 make $JOBS UI RES WIDGETS
 
 # Build discovery
-make $JOBS discovery EXTRA_LIBS="$EXTRA_LIBS1"
+make $JOBS discovery EXTRA_LIBS="$EXTRA_LIBS"
 mv source/discovery/carla-discovery-native.exe source/discovery/carla-discovery-win32.exe
 
 # Build backend
-make $JOBS backend EXTRA_LIBS="$EXTRA_LIBS1 $EXTRA_LIBS2"
+make $JOBS backend EXTRA_LIBS="$EXTRA_LIBS"
 
 # Build Plugin bridges
-# make $JOBS bridges EXTRA_LIBS="$EXTRA_LIBS1 $EXTRA_LIBS2"
+# make $JOBS bridges EXTRA_LIBS="$EXTRA_LIBS"
 
 # Build UI bridges
-make $JOBS -C source/bridges ui_lv2-win32 ui_vst-hwnd EXTRA_LIBS="$EXTRA_LIBS2"
+# make $JOBS -C source/bridges ui_lv2-win32 ui_vst-hwnd
 
 rm -rf ./data/windows/Carla
-cp ./source/carla.py ./source/carla.pyw
+cp ./source/carla ./source/carla.pyw
 $CXFREEZE --target-dir=".\\data\\windows\\Carla" ".\\source\\carla.pyw"
 rm -f ./source/carla.pyw
 
-rm -rf ./data/windows/CarlaControl
-cp ./source/carla_control.py ./source/carla_control.pyw
-$CXFREEZE --target-dir=".\\data\\windows\\CarlaControl" ".\\source\\carla_control.pyw"
-rm -f ./source/carla_control.pyw
+# rm -rf ./data/windows/CarlaControl
+# cp ./source/carla_control.py ./source/carla_control.pyw
+# $CXFREEZE --target-dir=".\\data\\windows\\CarlaControl" ".\\source\\carla_control.pyw"
+# rm -f ./source/carla_control.pyw
 
 cd data/windows
 mkdir Carla/backend
@@ -71,9 +70,9 @@ mkdir Carla/bridges
 mkdir Carla/discovery
 cp ../../source/backend/*.dll   Carla/backend/
 cp ../../source/discovery/*.exe Carla/discovery/
-mv CarlaControl/carla_control.exe CarlaControl/CarlaControl.exe
+# mv CarlaControl/carla_control.exe CarlaControl/CarlaControl.exe
 
-cp $WINEPREFIX/drive_c/windows/syswow64/python33.dll Carla/
+cp $WINEPREFIX/drive_c/windows/system32/python33.dll Carla/
 cp $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/QtCore4.dll   Carla/
 cp $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/QtGui4.dll    Carla/
 cp $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/QtOpenGL4.dll Carla/
@@ -82,26 +81,26 @@ cp $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/QtXml4.dll    Carla/
 cp -r $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/plugins/imageformats/ Carla/
 cp -r $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/plugins/iconengines/ Carla/
 
-cp $WINEPREFIX/drive_c/windows/syswow64/python33.dll CarlaControl/
-cp $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/QtCore4.dll   CarlaControl/
-cp $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/QtGui4.dll    CarlaControl/
-cp -r $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/plugins/imageformats/ CarlaControl/
-cp -r $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/plugins/iconengines/ CarlaControl/
+# cp $WINEPREFIX/drive_c/windows/system32/python33.dll CarlaControl/
+# cp $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/QtCore4.dll   CarlaControl/
+# cp $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/QtGui4.dll    CarlaControl/
+# cp -r $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/plugins/imageformats/ CarlaControl/
+# cp -r $WINEPREFIX/drive_c/Python33/Lib/site-packages/PyQt4/plugins/iconengines/ CarlaControl/
 
 # Build unzipfx
 make -C unzipfx-carla -f Makefile.win32
-make -C unzipfx-carla-control -f Makefile.win32
+# make -C unzipfx-carla-control -f Makefile.win32
 
 # Create static build
 rm -f Carla.zip CarlaControl.zip
 zip -r -9 Carla.zip Carla
-zip -r -9 CarlaControl.zip CarlaControl
+# zip -r -9 CarlaControl.zip CarlaControl
 
 rm -f Carla.exe CarlaControl.exe
 cat unzipfx-carla/unzipfx2cat.exe Carla.zip > Carla.exe
-cat unzipfx-carla-control/unzipfx2cat.exe CarlaControl.zip > CarlaControl.exe
+# cat unzipfx-carla-control/unzipfx2cat.exe CarlaControl.zip > CarlaControl.exe
 chmod +x Carla.exe
-chmod +x CarlaControl.exe
+# chmod +x CarlaControl.exe
 
 # Cleanup
 make -C unzipfx-carla -f Makefile.win32 clean
