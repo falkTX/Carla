5dc03b1a612543a9e4cf9fd80ede5c725d7f8726
falkTX <falktx@gmail.com>
2014-04-16 21:21:12 +0100

    Misc

--- source/modules/distrho/src/DistrhoPluginVST.cpp
+++ source/modules/distrho/src/DistrhoPluginVST.cpp
@@ -753,7 +753,7 @@ private:
         fPlugin.setState(newKey, newValue);
 
         // check if we want to save this key
-        if (! fPlugin.wantsStateKey(key))
+        if (! fPlugin.wantsStateKey(newKey))
             return;
 
         // check if key already exists
