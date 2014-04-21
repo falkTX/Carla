f243e9ed59b413ca82c74c2e3814fa0e7540da6b
falkTX <falktx@gmail.com>
2014-04-21 19:41:16 +0000

    Misc

--- data/windows/create-wineprefixes.sh
+++ data/windows/create-wineprefixes.sh
@@ -7,26 +7,26 @@ rm -rf ~/.winepy3_x64
 export WINEARCH=win32
 export WINEPREFIX=~/.winepy3_x86
 wineboot
-regsv32 wineasio.dll
+regsvr32 wineasio.dll
 winetricks corefonts
 winetricks fontsmooth=rgb
 
 # cd data/windows/python
 # msiexec /i python-3.3.5.msi /qn
-# wine PyQt5-5.2.1-gpl-Py3.3-Qt5.2.0-x32.exe
+# wine PyQt4-4.10.4-gpl-Py3.3-Qt4.8.5-x32.exe
 # msiexec /i cx_Freeze-4.3.2.win32-py3.3.msi /qn
 # cd ../../..
 
 export WINEARCH=win64
 export WINEPREFIX=~/.winepy3_x64
 wineboot
-regsv32 wineasio.dll
-wine64 regsv32 wineasio.dll
+regsvr32 wineasio.dll
+wine64 regsvr32 wineasio.dll
 winetricks corefonts
 winetricks fontsmooth=rgb
 
 # cd data/windows/python
 # msiexec /i python-3.3.5.amd64.msi /qn
-# wine PyQt5-5.2.1-gpl-Py3.3-Qt5.2.1-x64.exe
+# wine PyQt4-4.10.4-gpl-Py3.3-Qt4.8.5-x64.exe
 # msiexec /i cx_Freeze-4.3.2.win-amd64-py3.3.msi /qn
 # cd ../../..
