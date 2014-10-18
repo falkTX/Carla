3d66676e8e069de9332faf709edd1a58ed19880e
falkTX <falktx@gmail.com>
2014-10-18 22:30:12 +0100

    Update script again

--- data/linux/make-binary.sh
+++ data/linux/make-binary.sh
@@ -6,7 +6,7 @@ if [ -f Makefile ]; then
   cd data/linux
 fi
 
-VERSION="1.9.5~git20141017.6"
+VERSION="1.9.5~git20141017.10"
 
 if [ ! -f carla-git-static_"$VERSION"_amd64.deb ]; then
     wget -c https://launchpad.net/~kxstudio-debian/+archive/ubuntu/testing/+files/carla-git-static_"$VERSION"_amd64.deb
