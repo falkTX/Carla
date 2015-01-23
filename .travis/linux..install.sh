112538ee172d77f927940d1e29ce3b0efcdd6af1
falkTX <falktx@gmail.com>
2015-01-23 04:23:58 +0000

    Some travis experiments

---
+++ .travis/linux..install.sh
@@ -0,0 +1,7 @@
+sudo apt-get install -y pkg-config \
+    pyqt4-dev-tools libqt4-dev libgtk2.0-dev \
+    libasound2-dev libpulse-dev libmagic-dev libx11-dev libxft-dev \
+    libgl1-mesa-dev libglu1-mesa-dev \
+    liblo-static fftw3-static mxml-static zlib-static ntk-static \
+    libclthreads-static libclxclient-static zita-convolver-static zita-resampler-static \
+    fluidsynth-static linuxsampler-static
