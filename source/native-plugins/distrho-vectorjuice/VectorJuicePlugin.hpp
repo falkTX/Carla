3a1835781450c08856fcab06c10c7d98132bc652
falkTX <falktx@gmail.com>
2015-02-15 19:28:29 +0000

    Fix 32bit build again

--- source/native-plugins/distrho-vectorjuice/VectorJuicePlugin.hpp
+++ source/native-plugins/distrho-vectorjuice/VectorJuicePlugin.hpp
@@ -183,7 +183,7 @@ protected:
         return 0x1000;
     }
 
-    long d_getUniqueId() const noexcept override
+    int64_t d_getUniqueId() const noexcept override
     {
         return d_cconst('V', 'e', 'c', 'J');
     }
