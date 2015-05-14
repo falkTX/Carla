c51f4193e60dc483c7efb5e57fdc2a928fda947f
falkTX <falktx@gmail.com>
2015-05-14 20:18:10 +0200

    Skip building zynaddsubfx in travis for now

--- .travis/linux..install.sh
+++ .travis/linux..install.sh
@@ -2,6 +2,8 @@ sudo apt-get install -y pkg-config \
     pyqt4-dev-tools libqt4-dev libgtk2.0-dev \
     libasound2-dev libpulse-dev libmagic-dev libx11-dev libxft-dev \
     libgl1-mesa-dev libglu1-mesa-dev \
-    liblo-static fftw3-static mxml-static zlib-static ntk-static \
+    liblo-static \
     libclthreads-static libclxclient-static zita-convolver-static zita-resampler-static \
     fluidsynth-static linuxsampler-static
+
+# fftw3-static mxml-static zlib-static ntk-static
