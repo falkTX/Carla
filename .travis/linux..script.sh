c968994ea60430c7d089cfcd5304fbe737869e31
falkTX <falktx@gmail.com>
2015-05-15 11:51:34 +0200

    Fix travis build

--- .travis/linux..script.sh
+++ .travis/linux..script.sh
@@ -1,3 +1,6 @@
+export CFLAGS=-I/opt/kxstudio/include
+export CXXFLAGS=-I/opt/kxstudio/include
+export LDFLAGS=-L/opt/kxstudio/lib
 export PATH=/opt/kxstudio/bin:$PATH
 export PKG_CONFIG_PATH=/opt/kxstudio/lib/pkgconfig:$PKG_CONFIG_PATH
 
