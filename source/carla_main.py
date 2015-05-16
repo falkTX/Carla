2ad5196542d4cf19930769156f9b982ec8f8cc98
Adam Avramov <fallenblood@gmail.com>
2015-05-16 04:49:42 +0300

    Print usage info.

--- source/carla_main.py
+++ source/carla_main.py
@@ -22,12 +22,59 @@ from carla_host import (
     CarlaApplication, setUpSignals,
     initHost, loadHostSettings)
 
+
 # ------------------------------------------------------------------------------------------------------------
 # Constants
-MODE_HOST     = 0
-MODE_RACK     = 1
-MODE_PATCHBAY = 2
-MODE_CONTROL  = 3
+
+MODE_HOST     = "carla"
+MODE_RACK     = "carla-rack"
+MODE_PATCHBAY = "carla-patchbay"
+MODE_CONTROL  = "carla-control"
+
+
+# ------------------------------------------------------------------------------------------------------------
+# Print usage and/or version
+
+def version(libPrefix):
+    """ Print version information and exit. """
+    from carla_shared import VERSION, PYQT_VERSION_STR, qVersion, getPaths
+    pathBinaries, pathResources = getPaths(libPrefix)
+    print()
+    print("Carla %s" % VERSION)
+    print("  Python version: %s" % sys.version.split(" ",1)[0])
+    print("  Qt version:     %s" % qVersion())
+    print("  PyQt version:   %s" % PYQT_VERSION_STR)
+    print("  Binary dir:     %s" % pathBinaries)
+    print("  Resources dir:  %s" % pathResources)
+
+
+def usage(mode):
+    print()
+    if mode != MODE_CONTROL:
+        print("%s: JACK %s%s%s." % (
+            mode,
+            "patchbay"    if mode == MODE_PATCHBAY or mode == MODE_HOST else "",
+            " and "       if mode == MODE_HOST                          else "",
+            "plugin host" if mode == MODE_RACK     or mode == MODE_HOST else ""))
+        print()
+        print("Usage: %s [OPTION]... [FILE]" % mode)
+        print()
+        print("  where FILE can point to a Carla session file to be loaded")
+    else:
+        print("%s: Control a remote Carla instance via OSC." % mode)
+        print()
+        print("Usage: %s [OPTION]... [OSC_URL]" % mode)
+        print()
+        print("  where OSC_URL needs to be in format osc.udp://host:port")
+    print("  and OPTION can be one or more of the following:")
+    print()
+    print("  --version  Print version info and exit.")
+    print("  --usage    Print command line usage info and exit.")
+    print("  --help     Print both of the above, and exit.")
+    print()
+    print("  --with-appname=<name>    Set name of process.")
+    print("  --with-libprefix=<path>  Look for libraries in specified directory.")
+
 
 # ------------------------------------------------------------------------------------------------------------
 # Main
@@ -55,19 +102,20 @@ def start(mode = MODE_HOST):
 
     for arg in sys.argv:
         if arg == "--version":
-            # Print version information and exit.
-            from carla_shared import VERSION, PYQT_VERSION_STR, qVersion, getPaths
-            pathBinaries, pathResources = getPaths(libPrefix)
-            print("Carla %s" % VERSION)
-            print("  Python version: %s" % sys.version.split(" ",1)[0])
-            print("  Qt version:     %s" % qVersion())
-            print("  PyQt version:   %s" % PYQT_VERSION_STR)
-            print("  Binary dir:     %s" % pathBinaries)
-            print("  Resources dir:  %s" % pathResources)
+            version(libPrefix)
+            sys.exit(0)
+
+        elif arg == "--usage":
+            usage(mode)
+            sys.exit(0)
+
+        elif arg == "--help":
+            version(libPrefix)
+            usage(mode)
             sys.exit(0)
 
         elif arg.startswith("--with-appname="):
-            initName = os.path.basename(arg.replace("--with-initname=", ""))
+            initName = os.path.basename(arg.replace("--with-appname=", ""))
 
         elif arg.startswith("--with-libprefix="):
             libPrefix = arg.replace("--with-libprefix=", "")
