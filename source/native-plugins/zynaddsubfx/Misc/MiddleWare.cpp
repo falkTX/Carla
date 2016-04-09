a89c558e754146a631fa4ad6841b732f83e5f758
falkTX <falktx@gmail.com>
2016-04-09 23:15:00 +0200

    Testing, please ignore

--- source/native-plugins/zynaddsubfx/Misc/MiddleWare.cpp
+++ source/native-plugins/zynaddsubfx/Misc/MiddleWare.cpp
@@ -1362,6 +1362,8 @@ void MiddleWareImpl::sendToRemote(const char *rtmsg, std::string dest)
     if(dest == "GUI") {
         cb(ui, rtmsg);
     } else if(!dest.empty()) {
+        printf("Testing sendToRemote: '%s'\n", rtmsg);
+
         lo_message msg  = lo_message_deserialise((void*)rtmsg,
                 rtosc_message_length(rtmsg, bToU->buffer_size()), NULL);
 
