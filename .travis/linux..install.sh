5e3147eabad3af69dcfce8096907a0c9e155bb7d
falkTX <falktx@gmail.com>
2016-07-17 20:12:48 +0200

    Update travis stuff

--- .travis/linux..install.sh
+++ .travis/linux..install.sh
@@ -2,8 +2,6 @@ sudo apt-get install -y pkg-config \
     pyqt4-dev-tools libqt4-dev libgtk2.0-dev \
     libasound2-dev libpulse-dev libmagic-dev libx11-dev libxft-dev \
     libgl1-mesa-dev libglu1-mesa-dev \
-    liblo-static \
-    libclthreads-static libclxclient-static zita-convolver-static zita-resampler-static \
-    fluidsynth-static linuxsampler-static
-
-# fftw3-static mxml-static zlib-static ntk-static
+    liblo-static fftw3-static mxml-static zlib-static ntk-static \
+    fluidsynth-static linuxsampler-static \
+    libclthreads-static libclxclient-static zita-convolver-static zita-resampler-static
