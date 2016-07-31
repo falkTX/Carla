199078ef1eb88268d7305e48c6d4723bcf800ddc
falkTX <falktx@gmail.com>
2016-07-31 12:12:09 +0200

    Add travis files, imported from master

---
+++ .travis/linux..before_install.sh
@@ -0,0 +1,4 @@
+sudo add-apt-repository ppa:kxstudio-debian/kxstudio -y
+sudo apt-get update -qq
+sudo apt-get install kxstudio-repos
+sudo apt-get update -qq
---
+++ .travis/linux..install.sh
@@ -0,0 +1,6 @@
+sudo apt-get install -y pkg-config \
+    pyqt4-dev-tools libqt4-dev libgtk2.0-dev \
+    libasound2-dev libpulse-dev libmagic-dev libx11-dev libxft-dev \
+    libgl1-mesa-dev libglu1-mesa-dev \
+    liblo-static fftw3-static mxml-static zlib-static ntk-static \
+    fluidsynth-static linuxsampler-static
