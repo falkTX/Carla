bba132304d88fc870e68959d5f57c094575de634
falkTX <falktx@gmail.com>
2016-12-29 00:57:08 +0000

    Implement check for win32/64 binaries on windows Based on Ardour's dll_info function, thanks!

--- source/utils/CarlaBinaryUtils.hpp
+++ source/utils/CarlaBinaryUtils.hpp
@@ -21,8 +21,12 @@
 #include "CarlaBackend.h"
 #include "CarlaUtils.hpp"
 
-#if defined(HAVE_LIBMAGIC) && ! defined(BUILD_BRIDGE)
-# include <magic.h>
+#ifndef BUILD_BRIDGE
+# if defined(CARLA_OS_WIN)
+#  include "juce_core/juce_core.h"
+# elif defined(HAVE_LIBMAGIC)
+#  include <magic.h>
+# endif
 #endif
 
 CARLA_BACKEND_START_NAMESPACE
@@ -72,7 +76,53 @@ BinaryType getBinaryTypeFromFile(const char* const filename)
     if (filename == nullptr || filename[0] == '\0')
         return BINARY_NATIVE;
 
-#if defined(HAVE_LIBMAGIC) && ! defined(BUILD_BRIDGE)
+#ifndef BUILD_BRIDGE
+# if defined(CARLA_OS_WIN)
+    using juce::File;
+    using juce::FileInputStream;
+    using juce::ScopedPointer;
+
+    ScopedPointer<FileInputStream> stream(File(filename).createInputStream());
+    CARLA_SAFE_ASSERT_RETURN(stream != nullptr || stream->failedToOpen(), BINARY_NATIVE);
+
+    // -------------------------------------------------------------------
+    // binary type code based on Ardour's dll_info function
+    // See https://github.com/Ardour/ardour/blob/master/libs/ardour/plugin_manager.cc#L867,L925
+    // Copyright (C) 2000-2006 Paul Davis
+
+    uint8_t buf[68];
+
+    if (stream->read(buf, 68) != 68)
+        return BINARY_NATIVE;
+
+    if (buf[0] != 'M' && buf[1] != 'Z')
+        return BINARY_NATIVE;
+
+    const int32_t* const pe_hdr_off_ptr = (int32_t*)&buf[60];
+    const int32_t pe_hdr_off = *pe_hdr_off_ptr;
+
+    if (! stream->setPosition(pe_hdr_off))
+        return BINARY_NATIVE;
+
+    if (stream->read(buf, 6) != 6)
+        return BINARY_NATIVE;
+
+    if (buf[0] != 'P' && buf[1] != 'E')
+        return BINARY_NATIVE;
+
+    const uint16_t* const type_ptr = (uint16_t*)&buf[4];
+    const uint16_t type = *type_ptr;
+
+    switch (type)
+    {
+    case 0x014c:
+        return BINARY_WIN32;
+    case 0x8664:
+        return BINARY_WIN64;
+    default:
+        return BINARY_NATIVE;
+    }
+# elif defined(HAVE_LIBMAGIC)
     static const CarlaMagic magic;
 
     const char* const output(magic.getFileDescription(filename));
@@ -86,6 +136,7 @@ BinaryType getBinaryTypeFromFile(const char* const filename)
 
     if (std::strstr(output, "ELF") != nullptr)
         return (std::strstr(output, "x86-64") != nullptr || std::strstr(output, "aarch64") != nullptr) ? BINARY_POSIX64 : BINARY_POSIX32;
+# endif
 #endif
 
     return BINARY_NATIVE;
