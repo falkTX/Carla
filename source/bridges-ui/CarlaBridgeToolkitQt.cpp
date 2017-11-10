e6a06e6463cbb4c3d7c263a06c2ab8a3009654ec
falkTX <falktx@gmail.com>
2017-11-10 17:33:06 +0000

    Cleanup

--- source/bridges-ui/CarlaBridgeToolkitQt.cpp
+++ source/bridges-ui/CarlaBridgeToolkitQt.cpp
@@ -1,6 +1,6 @@
 /*
  * Carla Bridge Toolkit, Qt version
- * Copyright (C) 2011-2014 Filipe Coelho <falktx@falktx.com>
+ * Copyright (C) 2011-2017 Filipe Coelho <falktx@falktx.com>
  *
  * This program is free software; you can redistribute it and/or
  * modify it under the terms of the GNU General Public License as
@@ -19,7 +19,6 @@
 #include "CarlaBridgeToolkit.hpp"
 #include "CarlaStyle.hpp"
 
-#include <QtCore/QThread>
 #include <QtCore/QTimerEvent>
 
 #if QT_VERSION >= QT_VERSION_CHECK(5, 0, 0)
