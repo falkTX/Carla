6e06352880ad5736921c3ce24d88d5fbb1bf306a
falkTX <falktx@gmail.com>
2017-11-25 12:22:44 +0100

    Cleanup CI to remove experimental plugins (not used anymore)

--- .travis/linux..install.sh
+++ .travis/linux..install.sh
@@ -3,5 +3,4 @@ sudo apt-get install -y pkg-config \
     libasound2-dev libpulse-dev libmagic-dev libx11-dev libxft-dev \
     libgl1-mesa-dev libglu1-mesa-dev \
     liblo-static fftw3-static mxml-static zlib-static ntk-static \
-    fluidsynth-static linuxsampler-static \
-    libclthreads-static libclxclient-static zita-convolver-static zita-resampler-static
+    fluidsynth-static linuxsampler-static
--- .travis/linux..script.sh
+++ .travis/linux..script.sh
@@ -5,4 +5,4 @@ export PATH=/opt/kxstudio/bin:$PATH
 export PKG_CONFIG_PATH=/opt/kxstudio/lib/pkgconfig:$PKG_CONFIG_PATH
 
 make features
-make EXPERIMENTAL_PLUGINS=true
+make
