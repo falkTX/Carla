a8135c0ab94d924ec5945a8b32dea2fcd5207941
falkTX <falktx@gmail.com>
2018-01-13 21:26:48 +0100

    Update copyright year

--- source/carla_widgets.py
+++ source/carla_widgets.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # Carla widgets code
-# Copyright (C) 2011-2017 Filipe Coelho <falktx@falktx.com>
+# Copyright (C) 2011-2018 Filipe Coelho <falktx@falktx.com>
 #
 # This program is free software; you can redistribute it and/or
 # modify it under the terms of the GNU General Public License as
@@ -76,7 +76,7 @@ class CarlaAboutW(QDialog):
         self.ui.l_about.setText(self.tr(""
                                      "<br>Version %s"
                                      "<br>Carla is a fully-featured audio plugin host%s.<br>"
-                                     "<br>Copyright (C) 2011-2017 falkTX<br>"
+                                     "<br>Copyright (C) 2011-2018 falkTX<br>"
                                      "" % (VERSION, extraInfo)))
 
         if host.isControl:
