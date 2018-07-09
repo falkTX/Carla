73c765d16e7cf3815d899faf93340d89beb9ff3c
falkTX <falktx@gmail.com>
2018-07-10 00:42:30 +0200

    Add script to build and zip all windows releases

---
+++ data/windows/build-win-all.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+VERSION="2.0-beta7"
+
+# ---------------------------------------------------------------------------------------------------------------------
+# stop on error
+
+set -e
+
+# ---------------------------------------------------------------------------------------------------------------------
+# cd to correct path
+
+if [ ! -f Makefile ]; then
+  cd $(dirname $0)/../..
+fi
+
+# ---------------------------------------------------------------------------------------------------------------------
+# start
+
+make distclean
+pushd data/windows
+./build-win.sh 32nosse
+./pack-win.sh 32
+mv Carla_${VERSION}-win32.zip Carla_${VERSION}-win32-nosse.zip
+popd
+
+make distclean
+pushd data/windows
+./build-win.sh 32
+./pack-win.sh 32
+popd
+
+make distclean
+pushd data/windows
+./build-win.sh 64
+./pack-win.sh 64
+popd
+
+# ---------------------------------------------------------------------------------------------------------------------
