7325ce33f403016daeda8c254b4148742ed9d717
Nikita Zlobin <nick87720z@gmail.com>
2018-10-10 15:00:58 +0500

    rack list: Colorized padding

--- source/frontend/carla_host.py
+++ source/frontend/carla_host.py
@@ -27,12 +27,12 @@ from carla_config import *
 import json
 
 if config_UseQt5:
-    from PyQt5.QtCore import qCritical, QEventLoop, QFileInfo, QModelIndex, QPointF, QTimer
-    from PyQt5.QtGui import QImage, QPalette
+    from PyQt5.QtCore import qCritical, QEventLoop, QFileInfo, QModelIndex, QPointF, QTimer, QEvent
+    from PyQt5.QtGui import QImage, QPalette, QBrush
     from PyQt5.QtWidgets import QAction, QApplication, QInputDialog, QFileSystemModel, QListWidgetItem, QMainWindow
 else:
-    from PyQt4.QtCore import qCritical, QEventLoop, QFileInfo, QModelIndex, QPointF, QTimer
-    from PyQt4.QtGui import QImage, QPalette
+    from PyQt4.QtCore import qCritical, QEventLoop, QFileInfo, QModelIndex, QPointF, QTimer, QEvent
+    from PyQt4.QtGui import QImage, QPalette, QBrush
     from PyQt4.QtGui import QAction, QApplication, QInputDialog, QFileSystemModel, QListWidgetItem, QMainWindow
 
 # ------------------------------------------------------------------------------------------------------------
@@ -358,15 +358,9 @@ class HostWindow(QMainWindow):
         self.ui.rackScrollBar.rangeChanged.connect(sb.setRange)
         self.ui.rackScrollBar.valueChanged.connect(sb.setValue)
 
+        self.updateStyle()
+
         self.ui.rack.setStyleSheet("""
-          QLabel#pad_left {
-            background-image: url(:/bitmaps/rack_padding_left.png);
-            background-repeat: repeat-y;
-          }
-          QLabel#pad_right {
-            background-image: url(:/bitmaps/rack_padding_right.png);
-            background-repeat: repeat-y;
-          }
           CarlaRackList#CarlaRackList {
             background-color: black;
           }
@@ -2319,6 +2313,52 @@ class HostWindow(QMainWindow):
             self.idleSlow()
 
         QMainWindow.timerEvent(self, event)
+
+    # --------------------------------------------------------------------------------------------------------
+    # color/style change event
+
+    def changeEvent(self, event):
+        if event.type() in (QEvent.PaletteChange, QEvent.StyleChange):
+            self.updateStyle()
+        QWidget.changeEvent(self, event)
+
+    def updateStyle(self):
+        # Rack padding images setup
+        rack_imgL = QImage(":/bitmaps/rack_padding_left.png")
+        rack_imgR = QImage(":/bitmaps/rack_padding_right.png")
+
+        rack_imgV = 1.0-rack_imgL.scaled(1, 1, Qt.IgnoreAspectRatio, Qt.SmoothTransformation).pixelColor(0,0).blackF()
+        r0,g0,b0,a = self.ui.rack.palette().window().color().getRgbF()
+        r1,g1,b1,a = self.ui.rack.palette().text().color().getRgbF()
+
+        pad_color = QColor.fromRgbF(min(1.0, (r0*3+r1)/4),
+                                    min(1.0, (g0*3+g1)/4),
+                                    min(1.0, (b0*3+b1)/4))
+
+        painter = QPainter()
+        fillRect = rack_imgL.rect().adjusted(-1,-1,1,1)
+
+        painter.begin(rack_imgL)
+        painter.setCompositionMode(QPainter.CompositionMode_Multiply)
+        painter.setBrush(pad_color)
+        painter.drawRect(fillRect)
+        painter.end()
+        rack_pixmapL = QPixmap(rack_imgL)
+        self.imgL_palette = QPalette()
+        self.imgL_palette.setBrush(QPalette.Window, QBrush(rack_pixmapL))
+        self.ui.pad_left.setPalette(self.imgL_palette)
+        self.ui.pad_left.setAutoFillBackground(True)
+
+        painter.begin(rack_imgR)
+        painter.setCompositionMode(QPainter.CompositionMode_Multiply)
+        painter.setBrush(pad_color)
+        painter.drawRect(fillRect)
+        painter.end()
+        rack_pixmapR = QPixmap(rack_imgR)
+        self.imgR_palette = QPalette()
+        self.imgR_palette.setBrush(QPalette.Window, QBrush(rack_pixmapR))
+        self.ui.pad_right.setPalette(self.imgR_palette)
+        self.ui.pad_right.setAutoFillBackground(True)
 
     # --------------------------------------------------------------------------------------------------------
     # paint event
