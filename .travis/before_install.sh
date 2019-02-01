d3f850618a666042739c8b85b36af3c1be887116
falkTX <falktx@gmail.com>
2019-02-01 12:57:29 +0100

    Fixes and debug
    Signed-off-by: falkTX <falktx@gmail.com>

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -2,9 +2,14 @@
 
 set -e
 
-sudo add-apt-repository ppa:kxstudio-debian/kxstudio -y
-sudo add-apt-repository ppa:kxstudio-debian/mingw -y
-sudo add-apt-repository ppa:kxstudio-debian/toolchain -y
+sudo add-apt-repository -y ppa:kxstudio-debian/kxstudio
+sudo add-apt-repository -y ppa:kxstudio-debian/mingw
+sudo add-apt-repository -y ppa:kxstudio-debian/toolchain
+
+if [ "${TARGET}" = "linux-strict" ]; then
+  sudo add-apt-repository -y ubuntu-toolchain-r-test
+fi
+
 sudo apt-get update -qq
 sudo apt-get install kxstudio-repos
 sudo apt-get update -qq
