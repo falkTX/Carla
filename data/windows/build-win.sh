a20036e1b0087858d91de46c9feecfb69619dcc9
falkTX <falktx@falktx.com>
2020-04-15 21:35:44 +0100

    Do not use wine for native win32 builds
    Signed-off-by: falkTX <falktx@falktx.com>

--- data/windows/build-win.sh
+++ data/windows/build-win.sh
@@ -33,7 +33,7 @@ if [ x"${ARCH}" = x"32nosse" ]; then
   MAKE_ARGS="${MAKE_ARGS} NOOPT=true"
 fi
 
-MAKE_ARGS="${MAKE_ARGS} BUILDING_FOR_WINDOWS=true USING_JUCE=true"
+MAKE_ARGS="${MAKE_ARGS} BUILDING_FOR_WINDOWS=true CROSS_COMPILING=true"
 
 export WIN32=true
 
