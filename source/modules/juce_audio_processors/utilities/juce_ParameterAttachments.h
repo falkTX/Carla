0bd8c8175d8f6b9e026b25805da3f5b9d94a5538
falkTX <falktx@falktx.com>
2020-07-02 21:01:10 +0100

    Update juce sources to stable juce6 release (DISTRHO version)
    Signed-off-by: falkTX <falktx@falktx.com>

--- source/modules/juce_audio_processors/utilities/juce_ParameterAttachments.h
+++ source/modules/juce_audio_processors/utilities/juce_ParameterAttachments.h
@@ -1,13 +1,20 @@
 /*
   ==============================================================================
 
-   This file is part of the JUCE 6 technical preview.
+   This file is part of the JUCE library.
    Copyright (c) 2020 - Raw Material Software Limited
 
-   You may use this code under the terms of the GPL v3
-   (see www.gnu.org/licenses).
+   JUCE is an open source library subject to commercial or open-source
+   licensing.
 
-   For this technical preview, this file is not subject to commercial licensing.
+   By using JUCE, you agree to the terms of both the JUCE 6 End-User License
+   Agreement and JUCE Privacy Policy (both effective as of the 16th June 2020).
+
+   End User License Agreement: www.juce.com/juce-6-licence
+   Privacy Policy: www.juce.com/juce-privacy-policy
+
+   Or: You may also use this code under the terms of the GPL v3 (see
+   www.gnu.org/licenses).
 
    JUCE IS PROVIDED "AS IS" WITHOUT ANY WARRANTY, AND ALL WARRANTIES, WHETHER
    EXPRESSED OR IMPLIED, INCLUDING MERCHANTABILITY AND FITNESS FOR PURPOSE, ARE
@@ -33,6 +40,8 @@ namespace juce
 
     Make sure to call `sendInitialUpdate` at the end of your new attachment's
     constructor, so that the UI immediately reflects the state of the parameter.
+
+    @tags{Audio}
 */
 class ParameterAttachment   : private AudioProcessorParameter::Listener,
                               private AsyncUpdater
@@ -49,7 +58,7 @@ public:
                                           requests a parameter change.
     */
     ParameterAttachment (RangedAudioParameter& parameter,
-                         std::function<void(float)> parameterChangedCallback,
+                         std::function<void (float)> parameterChangedCallback,
                          UndoManager* undoManager = nullptr);
 
     /** Destructor. */
@@ -105,7 +114,7 @@ private:
     RangedAudioParameter& parameter;
     std::atomic<float> lastValue { 0.0f };
     UndoManager* undoManager = nullptr;
-    std::function<void(float)> setValue;
+    std::function<void (float)> setValue;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ParameterAttachment)
 };
@@ -118,6 +127,8 @@ private:
     it easy to connect a slider to a parameter. When this object is deleted, the
     connection is broken. Make sure that your parameter and Slider are not
     deleted before this object!
+
+    @tags{Audio}
 */
 class SliderParameterAttachment   : private Slider::Listener
 {
@@ -164,6 +175,8 @@ private:
     easy to connect a combo box to a parameter. When this object is deleted, the
     connection is broken. Make sure that your parameter and ComboBox are not deleted
     before this object!
+
+    @tags{Audio}
 */
 class ComboBoxParameterAttachment   : private ComboBox::Listener
 {
@@ -202,6 +215,8 @@ private:
     easy to connect a button to a parameter. When this object is deleted, the
     connection is broken. Make sure that your parameter and Button are not deleted
     before this object!
+
+    @tags{Audio}
 */
 class ButtonParameterAttachment   : private Button::Listener
 {
