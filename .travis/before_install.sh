92064ca85aeacb2278b968b4c547ef8ad9c82dfb
falkTX <falktx@falktx.com>
2020-07-25 17:41:19 +0100

    CI: Need to install wine on main linux build too, checking bridges

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -12,7 +12,7 @@ sudo add-apt-repository -y ppa:kxstudio-debian/toolchain
 
 if [ "${TARGET}" = "linux-strict" ] || [ "${TARGET}" = "linux-juce-strict" ]; then
     sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-elif [ "${TARGET}" = "win32" ] || [ "${TARGET}" = "win64" ]; then
+elif [ "${TARGET}" = "linux" ] || [ "${TARGET}" = "win32" ] || [ "${TARGET}" = "win64" ]; then
     wget -qO- https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
     sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
 fi
