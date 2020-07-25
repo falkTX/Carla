473a8977ff91ed89d9d7d055ae4425dd838a6bfc
falkTX <falktx@falktx.com>
2020-07-25 18:04:14 +0100

    CI fixup before-install

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -2,19 +2,17 @@
 
 set -e
 
-if [ "${TARGET}" = "linux" ] || [ "${TARGET}" = "win32" ]; then
-    sudo dpkg --add-architecture i386
-fi
-
 sudo add-apt-repository -y ppa:kxstudio-debian/kxstudio
 sudo add-apt-repository -y ppa:kxstudio-debian/mingw
 sudo add-apt-repository -y ppa:kxstudio-debian/toolchain
 
-if [ "${TARGET}" = "linux-strict" ] || [ "${TARGET}" = "linux-juce-strict" ]; then
-    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
-elif [ "${TARGET}" = "linux" ] || [ "${TARGET}" = "win32" ] || [ "${TARGET}" = "win64" ]; then
+if [ "${TARGET}" = "linux" ]; then
     wget -qO- https://dl.winehq.org/wine-builds/winehq.key | sudo apt-key add -
-    sudo apt-add-repository 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
+    sudo apt-add-repository -y 'deb https://dl.winehq.org/wine-builds/ubuntu/ bionic main'
+    sudo add-apt-repository -y ppa:kxstudio-debian/ubuntus
+    sudo dpkg --add-architecture i386
+elif [ "${TARGET}" = "linux-strict" ] || [ "${TARGET}" = "linux-juce-strict" ]; then
+    sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 fi
 
 sudo apt-get update -qq
