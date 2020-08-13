698d42656fb19613af2dbe08e19c4c0471e76e9f
falkTX <falktx@falktx.com>
2020-08-13 05:40:22 +0100

    Finalize XY controller plugin
    Signed-off-by: falkTX <falktx@falktx.com>

--- source/native-plugins/xycontroller.cpp
+++ source/native-plugins/xycontroller.cpp
@@ -17,7 +17,11 @@
 
 #include "CarlaDefines.h"
 
+#include "CarlaMIDI.h"
 #include "CarlaNativeExtUI.hpp"
+
+#include "midi-queue.hpp"
+#include "water/text/StringArray.h"
 
 // -----------------------------------------------------------------------
 
@@ -34,9 +38,14 @@ public:
 
     XYControllerPlugin(const NativeHostDescriptor* const host)
         : NativePluginAndUiClass(host, "xycontroller-ui"),
-          fParams()
+          params(),
+          channels(),
+          mqueue(),
+          mqueueRT()
     {
-        carla_zeroStruct(fParams);
+        carla_zeroStruct(params);
+        carla_zeroStruct(channels);
+        channels[0] = true;
     }
 
 protected:
@@ -94,7 +103,7 @@ protected:
     {
         CARLA_SAFE_ASSERT_RETURN(index < kParamCount, 0.0f);
 
-        return fParams[index];
+        return params[index];
     }
 
     // -------------------------------------------------------------------
@@ -106,8 +115,29 @@ protected:
         {
         case kParamInX:
         case kParamInY:
-            fParams[index] = value;
+            params[index] = value;
             break;
+        }
+    }
+
+    void setCustomData(const char* const key, const char* const value) override
+    {
+        CARLA_SAFE_ASSERT_RETURN(key != nullptr && key[0] != '\0',);
+        CARLA_SAFE_ASSERT_RETURN(value != nullptr,);
+
+        if (std::strcmp(key, "channels") == 0)
+        {
+            const water::StringArray chans(water::StringArray::fromTokens(value, ",", ""));
+
+            carla_zeroStruct(channels);
+
+            for (const water::String chan : chans)
+            {
+                const int ichan = std::atoi(chan.toRawUTF8());
+                CARLA_SAFE_ASSERT_INT_CONTINUE(ichan >= 1 && ichan <= 16, ichan);
+
+                channels[ichan-1] = true;
+            }
         }
     }
 
@@ -115,14 +145,109 @@ protected:
     // Plugin process calls
 
     void process(const float* const*, float**, const uint32_t,
-                 const NativeMidiEvent* const, const uint32_t) override
+                 const NativeMidiEvent* const midiEvents, const uint32_t midiEventCount) override
     {
-        fParams[kParamOutX] = fParams[kParamInX];
-        fParams[kParamOutY] = fParams[kParamInY];
+        params[kParamOutX] = params[kParamInX];
+        params[kParamOutY] = params[kParamInY];
+
+        if (mqueue.isNotEmpty() && mqueueRT.tryToCopyDataFrom(mqueue))
+        {
+            uint8_t d1, d2, d3;
+            NativeMidiEvent ev = { 0, 0, 3, { 0, 0, 0, 0 } };
+
+            while (mqueueRT.get(d1, d2, d3))
+            {
+                ev.data[0] = d1;
+                ev.data[1] = d2;
+                ev.data[2] = d3;
+                writeMidiEvent(&ev);
+            }
+        }
+
+        for (uint32_t i=0; i < midiEventCount; ++i)
+            writeMidiEvent(&midiEvents[i]);
+    }
+
+    // -------------------------------------------------------------------
+    // Pipe Server calls
+
+    bool msgReceived(const char* const msg) noexcept override
+    {
+        if (NativePluginAndUiClass::msgReceived(msg))
+            return true;
+
+        if (std::strcmp(msg, "cc") == 0)
+        {
+            uint8_t cc, value;
+            CARLA_SAFE_ASSERT_RETURN(readNextLineAsByte(cc), true);
+            CARLA_SAFE_ASSERT_RETURN(readNextLineAsByte(value), true);
+
+            const CarlaMutexLocker cml(mqueue.getMutex());
+
+            for (int i=0; i<16; ++i)
+            {
+                if (channels[i])
+                    if (! mqueue.put(uint8_t(MIDI_STATUS_CONTROL_CHANGE | (i & MIDI_CHANNEL_BIT)), cc, value))
+                        break;
+            }
+
+            return true;
+        }
+
+        if (std::strcmp(msg, "cc2") == 0)
+        {
+            uint8_t cc1, value1, cc2, value2;
+            CARLA_SAFE_ASSERT_RETURN(readNextLineAsByte(cc1), true);
+            CARLA_SAFE_ASSERT_RETURN(readNextLineAsByte(value1), true);
+            CARLA_SAFE_ASSERT_RETURN(readNextLineAsByte(cc2), true);
+            CARLA_SAFE_ASSERT_RETURN(readNextLineAsByte(value2), true);
+
+            const CarlaMutexLocker cml(mqueue.getMutex());
+
+            for (int i=0; i<16; ++i)
+            {
+                if (channels[i])
+                {
+                    if (! mqueue.put(uint8_t(MIDI_STATUS_CONTROL_CHANGE | (i & MIDI_CHANNEL_BIT)), cc1, value1))
+                        break;
+                    if (! mqueue.put(uint8_t(MIDI_STATUS_CONTROL_CHANGE | (i & MIDI_CHANNEL_BIT)), cc2, value2))
+                        break;
+                }
+            }
+
+            return true;
+        }
+
+        if (std::strcmp(msg, "note") == 0)
+        {
+            bool onOff;
+            uint8_t note;
+            CARLA_SAFE_ASSERT_RETURN(readNextLineAsBool(onOff), true);
+            CARLA_SAFE_ASSERT_RETURN(readNextLineAsByte(note), true);
+
+            const uint8_t status   = onOff ? MIDI_STATUS_NOTE_ON : MIDI_STATUS_NOTE_OFF;
+            const uint8_t velocity = onOff ? 100 : 0;
+
+            const CarlaMutexLocker cml(mqueue.getMutex());
+
+            for (int i=0; i<16; ++i)
+            {
+                if (channels[i])
+                    if (! mqueue.put(uint8_t(status | (i & MIDI_CHANNEL_BIT)), note, velocity))
+                        break;
+            }
+
+            return true;
+        }
+
+        return false;
     }
 
 private:
-    float fParams[kParamCount];
+    float params[kParamCount];
+    bool channels[16];
+
+    MIDIEventQueue<128> mqueue, mqueueRT;
 
     PluginClassEND(XYControllerPlugin)
     CARLA_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR(XYControllerPlugin)
@@ -137,7 +262,7 @@ static const NativePluginDescriptor notesDesc = {
     /* supports  */ NATIVE_PLUGIN_SUPPORTS_NOTHING,
     /* audioIns  */ 0,
     /* audioOuts */ 0,
-    /* midiIns   */ 0,
+    /* midiIns   */ 1,
     /* midiOuts  */ 1,
     /* paramIns  */ 2,
     /* paramOuts */ 2,
