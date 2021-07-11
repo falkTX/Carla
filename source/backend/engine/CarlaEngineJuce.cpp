7b29441926be9544e0e48f951af601647cc3f61b
falkTX <falktx@falktx.com>
2021-07-11 17:22:39 +0100

    Fix build
    Signed-off-by: falkTX <falktx@falktx.com>

--- source/backend/engine/CarlaEngineJuce.cpp
+++ source/backend/engine/CarlaEngineJuce.cpp
@@ -856,7 +856,7 @@ private:
 
         RtMidiEvents()
             : mutex(),
-              dataPool(512, 512),
+              dataPool("RtMidiEvents", 512, 512),
               data(dataPool),
               dataPending(dataPool) {}
 
