1d154adbf277d63cf1d2d3a0fea68f7a384a55aa
falkTX <falktx@falktx.com>
2022-10-12 02:36:27 +0100

    Add missing file
    Signed-off-by: falkTX <falktx@falktx.com>

---
+++ source/includes/clap/ext/latency.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "../plugin.h"
+
+static CLAP_CONSTEXPR const char CLAP_EXT_LATENCY[] = "clap.latency";
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+// The audio ports scan has to be done while the plugin is deactivated.
+typedef struct clap_plugin_latency {
+   // Returns the plugin latency.
+   // [main-thread]
+   uint32_t(CLAP_ABI *get)(const clap_plugin_t *plugin);
+} clap_plugin_latency_t;
+
+typedef struct clap_host_latency {
+   // Tell the host that the latency changed.
+   // The latency is only allowed to change if the plugin is deactivated.
+   // If the plugin is activated, call host->request_restart()
+   // [main-thread]
+   void(CLAP_ABI *changed)(const clap_host_t *host);
+} clap_host_latency_t;
+
+#ifdef __cplusplus
+}
+#endif
